23e2eb1a6325dad85722a785a78236640b709f0b
zngue <875407262@qq.com>
2020-04-07 13:09:31 +0800

    code

--- src/Commands/CodeGeneratorCommand.php
+++ src/Commands/CodeGeneratorCommand.php
@@ -7,6 +7,7 @@ namespace Zngue\CodeGenerator\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use Illuminate\Container\Container;
+use Zngue\User\Service\toolesService;
 
 class CodeGeneratorCommand extends Command
 {
@@ -20,28 +21,19 @@ class CodeGeneratorCommand extends Command
      */
     protected $description = 'Create a laralib scaffold';
     public function handle(){
-
-
-        $temp = __DIR__.'/../../template/Controller.tpl';
-
-        $name = "cate";
-        $key='name';
-
-
-
-        $temp= File::get($temp);
-
-        $template = str_replace("{{". $key ."}}", ucwords($name), $temp);
-
-        $savePath = __DIR__.'/../Http/Controller/User/';
-
-        if(!File::exists($savePath)){
-            File::makeDirectory($savePath,775);
-        }
-        File::put($savePath.ucwords($name).'Controller.php',$template);
-
-
-        echo 111;
+//        $this->call("vendor:publish",['--provider'=>'Zngue\User\UserService']);
+//        $this->call("vendor:publish",['--provider'=>'Zngue\Module\Provider\ModuleServiceProvider']);
+        $this->call('zng:module');
+        $data=array(
+            'dbhost'=>config('database.connections.mysql.host'),
+            'dbuser'=> config('database.connections.mysql.username'),
+            'dbpw'=>config('database.connections.mysql.password'),
+            'dbname'=>config('database.connections.mysql.database')
+        );
+        $file = __DIR__.'/../../sql/code.sql';
+        $tool=new toolesService($data);
+        $data =$tool->import_data($file,config('database.connections.mysql.prefix'));
+        echo $data['info'];
     }
 
 }
