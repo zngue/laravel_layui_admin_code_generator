af0ea44ad0712188284ea8d3ec624912030ec4c2
zngue <875407262@qq.com>
2020-04-07 13:54:45 +0800

    添加执行命令

--- src/Provider/CodeGeneratorServiceProvider.php
+++ src/Provider/CodeGeneratorServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Zngue\CodeGenerator\Provider;
 use Illuminate\Support\ServiceProvider;
+use Zngue\CodeGenerator\Commands\CodeGeneratorCommand;
 
 class CodeGeneratorServiceProvider extends ServiceProvider
 {
@@ -12,6 +13,12 @@ class CodeGeneratorServiceProvider extends ServiceProvider
 
         $this->loadRoutesFrom(__DIR__ . "/../../routes/web.php");
         $this->loadViewsFrom(__DIR__ . '/../../views', 'zng');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                CodeGeneratorCommand::class
+            ]);
+        }
 
     }
 }
